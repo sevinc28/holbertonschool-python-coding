8a0e05c70bc754dad60bea1d40d0b6a97db53757
sevinc28 <jhasimova28@gmail.com>
2025-05-12 16:46:46 +0400

    task0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-python3 -c "d = __import__('0-square').__doc__ ; r = 'OK\n' if d is not None and len(d.strip()) > 0 else '' ; print(r, end='')" | wc -l
+"""Module that defines a Square class."""
+
+class Square:
+    """Defines a square."""
+    def __init__(self, size):
+        self.__size = size
