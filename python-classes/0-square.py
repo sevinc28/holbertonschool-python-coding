f725830e49a650dd30003d6ffc45e1baabd42050
sevinc28 <jhasimova28@gmail.com>
2025-05-12 14:28:32 +0400

    task0

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
