5c534b77a2c14915466bf92447513ad05b406775
sevinc28 <jhasimova28@gmail.com>
2025-06-12 05:44:17 +0000

    task0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
-class Square:
-    pass
-    def __init__ (self, size):
-        self.__size = size
+Square = __import__('0-square').Square
 
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
