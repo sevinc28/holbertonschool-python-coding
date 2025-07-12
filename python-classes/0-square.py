835ddfedf9f7260cd121f9cd9226e51d8e8195fd
sevinc28 <jhasimova28@gmail.com>
2025-07-12 19:42:32 +0000

    task0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Kvadrat sinifin tesviri"""
+
+
 class Square:
+    """Olcusune gore kvadrati teyin eden sinif"""
     def __init__(self, __size):
         self.__size = __size
