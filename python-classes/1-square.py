0f1e88bb5833e75e4fff541911b9424192c56876
sevinc28 <jhasimova28@gmail.com>
2025-07-12 20:01:53 +0000

    task0

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,14 +1,12 @@
-#!/usr/bin/python3
-"""Square class definition"""
-
-
+#!/usr/bin/Python3
+"""Kvadrat adli sinif yaradiriq"""
 
 class Square:
-    """A class that defines a square by its size"""
+    """Olcusune gore kvadrat teyin edirik"""
 
     def __init__(self, size=0):
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("olcu tam eded olmalidir")
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("olcu 0 ve ya daha boyuk olmalidir")
         self.__size = size
